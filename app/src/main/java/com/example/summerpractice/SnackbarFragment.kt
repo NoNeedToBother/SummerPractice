37c9bb117d5be3559dcce6e5150320d4e3ba98f0
Dmitry Paramonov <dmitparam.2017@gmail.com>
2023-07-02 15:57:49 +0300

    bugfix

--- app/src/main/java/com/example/summerpractice/SnackbarFragment.kt
+++ app/src/main/java/com/example/summerpractice/SnackbarFragment.kt
@@ -3,6 +3,7 @@ package com.example.summerpractice
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.summerpractice.databinding.FragmentSettingsBinding
 import com.example.summerpractice.databinding.FragmentSnackbarBinding
 import com.google.android.material.snackbar.Snackbar
@@ -14,10 +15,11 @@ class SnackbarFragment : Fragment(R.layout.fragment_snackbar) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSnackbarBinding.bind(view)
-        val className = arguments?.getString("CLASS_NAME", "")
+        val className = arguments?.getString("CLASS_NAME")
         binding?.run {
             if (className != null) {
                 Snackbar.make(root, className, Snackbar.LENGTH_LONG).show()
+                findNavController().navigate(R.id.action_snackbarFragment_to_mainFragment)
             }
         }
 
